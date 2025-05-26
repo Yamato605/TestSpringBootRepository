68fb5486f6ddf4fbbb1f76f31d70c25038e701e2
Yamato605 <ymt-956@outlook.jp>
2025-05-26 10:35:23 +0900

    Apply .gitignore rules

--- src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java
+++ src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java
@@ -27,3 +27,46 @@ public class TestSpringBootApplication {
 	}
 
 }
+/*
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+新機能と改善のために最新の PowerShell をインストールしてください!https://aka.ms/PSWindows
+
+PS C:\Users\Owner\Downloads\TestSpringBoot\TestSpringBoot> git init
+Initialized empty Git repository in C:/Users/Owner/Downloads/TestSpringBoot/TestSpringBoot/.git/
+PS C:\Users\Owner\Downloads\TestSpringBoot\TestSpringBoot> git add .
+warning: in the working copy of '.gitattributes', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.gitignore', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'build.gradle', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'gradle/wrapper/gradle-wrapper.properties', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'settings.gradle', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'src/main/java/com/example/TestSpringBoot/ServletInitializer.java', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'src/main/resources/application.properties', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of 'src/test/java/com/example/TestSpringBoot/TestSpringBootApplicationTests.java', LF will be replaced by CRLF the next time Git touches it
+PS C:\Users\Owner\Downloads\TestSpringBoot\TestSpringBoot> git commit -m "first commit with .gitignore makefile"
+[master (root-commit) b2efbc3] first commit with .gitignore makefile
+ 13 files changed, 498 insertions(+)
+ create mode 100644 .gitattributes
+ create mode 100644 .gitignore
+ create mode 100644 build.gradle
+ create mode 100644 gradle/wrapper/gradle-wrapper.jar
+ create mode 100644 gradle/wrapper/gradle-wrapper.properties
+ create mode 100644 gradlew
+ create mode 100644 gradlew.bat
+ create mode 100644 settings.gradle
+ create mode 100644 src/main/java/com/example/TestSpringBoot/.gitignore
+ create mode 100644 src/main/java/com/example/TestSpringBoot/ServletInitializer.java
+ create mode 100644 src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java
+ create mode 100644 src/main/resources/application.properties
+ create mode 100644 src/test/java/com/example/TestSpringBoot/TestSpringBootApplicationTests.java
+PS C:\Users\Owner\Downloads\TestSpringBoot\TestSpringBoot> git branch -m master main
+PS C:\Users\Owner\Downloads\TestSpringBoot\TestSpringBoot> git push -u origin main
+fatal: 'origin' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+
+ */
